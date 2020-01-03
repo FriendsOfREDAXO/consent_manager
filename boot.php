8937c8e7527f8c407a4bf08220f67b2a65383af3
Ingo Winter <winteringo@gmail.com>
2020-01-03 19:55:10 +0100

    https://github.com/FriendsOfREDAXO/iwcc/issues/25

--- boot.php
+++ boot.php
@@ -15,7 +15,7 @@ rex_extension::register('PACKAGES_INCLUDED', function () {
             rex_file::copy($this->getPath('assets/iwcc_frontend.css'), $this->getAssetsPath('iwcc_frontend.css'));
             rex_file::copy($this->getPath('assets/iwcc_backend.css'), $this->getAssetsPath('iwcc_backend.css'));
             rex_file::copy($this->getPath('assets/iwcc_frontend.js'), $this->getAssetsPath('iwcc_frontend.js'));
-            rex_file::copy($this->getPath('assets/iwcc_backend.js'), $this->getAssetsPath('iwcc_backend.js'));
+            //rex_file::copy($this->getPath('assets/iwcc_backend.js'), $this->getAssetsPath('iwcc_backend.js'));
         }
         if (rex::isBackend())
         {
