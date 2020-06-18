9a5c34e8474c38a04204e16a71ec9640a32608e0
Tobias Krais <tk@design-to-use.de>
2020-06-18 12:17:06 +0200

    ID entfernt

--- update.php
+++ update.php
@@ -3,7 +3,7 @@ $sql = \rex_sql::factory();
 $sql->setQuery("SELECT * FROM ". \rex::getTablePrefix() ."iwcc_text WHERE `uid` = 'button_accept';");
 if($sql->getRows() == 0) {
     foreach(\rex_clang::getAllIds() as $clang_id) {
-	    $sql->setQuery("INSERT INTO `". \rex::getTablePrefix() ."iwcc_text` (`id`, `clang_id`, `uid`, `text`) VALUES (7, ". $clang_id .", 'button_accept', 'Auswahl bestätigen');");
+	    $sql->setQuery("INSERT INTO `". \rex::getTablePrefix() ."iwcc_text` (`clang_id`, `uid`, `text`) VALUES (". $clang_id .", 'button_accept', 'Auswahl bestätigen');");
     }
 }
 
@@ -11,6 +11,6 @@ $sql = \rex_sql::factory();
 $sql->setQuery("SELECT * FROM ". \rex::getTablePrefix() ."iwcc_text WHERE `uid` = 'button_select_all';");
 if($sql->getRows() == 0) {
     foreach(\rex_clang::getAllIds() as $clang_id) {
-	    $sql->setQuery("INSERT INTO `". \rex::getTablePrefix() ."iwcc_text` (`id`, `clang_id`, `uid`, `text`) VALUES (8, ". $clang_id .", 'button_select_all', 'Alle auswählen');");
+	    $sql->setQuery("INSERT INTO `". \rex::getTablePrefix() ."iwcc_text` (`clang_id`, `uid`, `text`) VALUES (". $clang_id .", 'button_select_all', 'Alle auswählen');");
     }
 }
