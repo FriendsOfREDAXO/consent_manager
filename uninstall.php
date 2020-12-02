e7b9d31057a56558af04bf2a45ee4e744276283f
tyrant88 <nm@lautundschoen.de>
2020-12-02 17:01:05 +0100

    fix Tabellenamen

--- uninstall.php
+++ uninstall.php
@@ -4,3 +4,5 @@ rex_sql_table::get(rex::getTable('consent_manager_cookiegroup'))->drop();
 rex_sql_table::get(rex::getTable('consent_manager_cookie'))->drop();
 rex_sql_table::get(rex::getTable('consent_manager_text'))->drop();
 rex_sql_table::get(rex::getTable('consent_manager_domain'))->drop();
+rex_sql_table::get(rex::getTable('consent_manager_cache_log'))->drop();
+rex_sql_table::get(rex::getTable('consent_manager_consent_log'))->drop();
