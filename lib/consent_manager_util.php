584af61af9295f003ca658184f7e15567874034e
aeberhard <andreaseberhard@gmail.com>
2023-04-29 23:28:48 +0200

    Update consent_manager_util.php
    Neue Methode `consent_manager_util::hostname()` - liefert Hostname ohne Subdomain und Port

--- lib/consent_manager_util.php
+++ lib/consent_manager_util.php
@@ -22,4 +22,22 @@ class consent_manager_util
         }
         return false;
     }
+
+    /**
+     * Hostname without subdomain and port.
+     * https:// onlinecode.org/php-get-domain-name-from-full-url-with-parameters-programming/
+     * @api
+     */
+    public static function hostname(): string
+    {
+        $parts = parse_url('https://' . rex_request::server('HTTP_HOST'));
+        //$parts = parse_url('http://' . 'test.aesoft.de/foo/bar'); // test
+        //$parts = parse_url('http://' . 'mail.onlinecode.co.uk'); // test
+        $domain = $parts['host'] ?? '';
+        if (false !== preg_match('/(?P<domain>[a-z0-9][a-z0-9-]{1,63}.[a-z.]{2,6})$/i', $domain, $regs)) {
+            return $regs['domain'];
+        }
+        return ''.rex_request::server('HTTP_HOST');
+    }
+
 }
