35d13ad3161d0c3449bcb0a726a011b6b7bdb22d
aeberhard <andreaseberhard@gmail.com>
2023-06-05 00:52:36 +0200

    Fix whoops wenn das project-Addon physisch nicht existiert

--- pages/theme.php
+++ pages/theme.php
@@ -40,7 +40,7 @@ if ('' !== $preview) {
 }
 html {
     min-height: 100%;
-    background-image: url(../assets/addons/consent_manager/<?php echo $backgroundimage; ?>);
+    background-image: url(../assets/addons/consent_manager/<?= $backgroundimage ?>);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center center;
@@ -77,16 +77,16 @@ div.theme_description span {
 </head>
 <body>
 
-<h1 id="previewtitle"> <?php echo $theme_options['name'] ?? $preview; ?></h1>
+<h1 id="previewtitle"> <?= $theme_options['name'] ?? $preview ?></h1>
 
 <?php if (false !== $cmstyle && [] !== $theme_options) { ?>
 <div class="theme_description">
-<p><span><?php echo $addon->i18n('theme_name') ?></span><?php echo $theme_options['name']; ?></p>
-<p><span><?php echo $addon->i18n('theme_description') ?></span><?php echo $theme_options['description']; ?></p>
-<p><span><?php echo $addon->i18n('theme_type') ?></span><?php echo $theme_options['type']; ?></p>
-<p><span><?php echo $addon->i18n('theme_style') ?></span><?php echo $theme_options['style']; ?></p>
-<p><span><?php echo $addon->i18n('theme_scssfile') ?></span><?php echo $preview; ?></p>
-<p><span><?php echo $addon->i18n('theme_autor') ?></span><?php echo $theme_options['autor']; ?></p>
+<p><span><?= $addon->i18n('theme_name') ?></span><?= $theme_options['name'] ?></p>
+<p><span><?= $addon->i18n('theme_description') ?></span><?= $theme_options['description'] ?></p>
+<p><span><?= $addon->i18n('theme_type') ?></span><?= $theme_options['type'] ?></p>
+<p><span><?= $addon->i18n('theme_style') ?></span><?= $theme_options['style'] ?></p>
+<p><span><?= $addon->i18n('theme_scssfile') ?></span><?= $preview ?></p>
+<p><span><?= $addon->i18n('theme_autor') ?></span><?= $theme_options['autor'] ?></p>
 </div>
 
 <?php } ?>
@@ -224,7 +224,7 @@ if (count($themes) > 0) {
         $class = '';
         $titleactive = '';
 
-        $themeid = basename(strval($themefile));
+        $themeid = basename((string) $themefile);
         $theme_options = $cmtheme->getThemeInformation($themeid);
 
         if (count($theme_options) > 0) {
@@ -308,94 +308,96 @@ if (count($themes) > 0) {
 }
 
 // Themes im project-Addon
-$themes = (array) glob(rex_addon::get('project')->getPath('consent_manager_themes/consent_manager_frontend*.scss'));
-natsort($themes);
-if (count($themes) > 0) {
-    echo '<h2>' . $addon->i18n('themes_project_addon') . '</h2>';
-    echo '<p>' . $addon->i18n('themes_project_addon_info') . '</p>';
-    $cmtheme = new consent_manager_theme();
-    foreach ($themes as $themefile) {
-        $output = '';
-        $content = '';
-        $class = '';
-        $titleactive = '';
+if (true === rex_addon::exists('project')) {
+    $themes = (array) glob(rex_addon::get('project')->getPath('consent_manager_themes/consent_manager_frontend*.scss'));
+    natsort($themes);
+    if (count($themes) > 0) {
+        echo '<h2>' . $addon->i18n('themes_project_addon') . '</h2>';
+        echo '<p>' . $addon->i18n('themes_project_addon_info') . '</p>';
+        $cmtheme = new consent_manager_theme();
+        foreach ($themes as $themefile) {
+            $output = '';
+            $content = '';
+            $class = '';
+            $titleactive = '';
 
-        $themeid = 'project:' . basename(strval($themefile));
-        $theme_options = $cmtheme->getThemeInformation($themeid);
-        if (count($theme_options) > 0) {
-            $authorlinks = [];
-            $authors = explode(',', $theme_options['autor']);
-            foreach ($authors as $author) {
-                $url = 'https://github.com/' . str_replace('@', '', trim($author));
-                $authorlinks[] = '<a href="'.$url.'" target="_blank">'.$author.'</a>';
+            $themeid = 'project:' . basename((string) $themefile);
+            $theme_options = $cmtheme->getThemeInformation($themeid);
+            if (count($theme_options) > 0) {
+                $authorlinks = [];
+                $authors = explode(',', $theme_options['autor']);
+                foreach ($authors as $author) {
+                    $url = 'https://github.com/' . str_replace('@', '', trim($author));
+                    $authorlinks[] = '<a href="'.$url.'" target="_blank">'.$author.'</a>';
+                }
+
+                $formElements = [];
+
+                $n = [];
+                $n['field'] = '';
+
+                $n['field'] .= '<p class="theme_description">';
+                $n['field'] .= '<span>' . $addon->i18n('theme_name') . '</span> ' . $theme_options['name'] . '<br>';
+                $n['field'] .= '<span>' . $addon->i18n('theme_description') . '</span> ' . $theme_options['description'] . '<br>';
+                $n['field'] .= '<span>' . $addon->i18n('theme_type') . '</span> ' . $theme_options['type'] . '<br>';
+                $n['field'] .= '<span>' . $addon->i18n('theme_style') . '</span> ' . $theme_options['style'] . '<br>';
+                $n['field'] .= '<span>' . $addon->i18n('theme_scssfile') . '</span> ' . $themeid . '<br>';
+                $n['field'] .= '<span>' . $addon->i18n('theme_autor') . '</span> ' . implode(', ', $authorlinks) . '<br>';
+                $n['field'] .= '</p>';
+
+                $n['field'] .= '<div class="thumbnail-container" title="' .  $theme_options['name'] . '" data-theme="' . $themeid . '">';
+                $n['field'] .= '  <div class="thumbnail">';
+                $n['field'] .= '   <iframe loading="lazy" width="1440px" height="900px" class="thumbnailframe" src="?page=consent_manager/theme&preview='.$themeid.'&nofocus" data-theme="' . $themeid . '" onload="this.style.opacity = 1"></iframe>';
+                $n['field'] .= '  </div>';
+                $n['field'] .= '</div>';
+
+                $formElements[] = $n;
+
+                $fragment = new rex_fragment();
+                $fragment->setVar('elements', $formElements, false);
+                $content = $fragment->parse('core/form/form.php');
+
+                // Buttons
+                $confirmmsg = $addon->i18n('config_confirm_select', $theme_options['name']);
+                $formElements = [];
+                $n = [];
+                $n['field'] = '<a href="?page=consent_manager/theme&preview=' . $themeid . '" class="btn rex-form-aligned btn-info consent_manager-button-preview" data-theme="' . $themeid . '">' . $addon->i18n('config_btn_preview') . '</a> ';
+                $n['field'] .= '<button class="btn btn-save" type="submit" name="save" data-confirm="' . $confirmmsg . '" value="' . $addon->i18n('config_btn_select', $theme_options['name']) . '">' . $addon->i18n('config_btn_select', $theme_options['name']) . '</button>';
+                $formElements[] = $n;
+                $fragment = new rex_fragment();
+                $fragment->setVar('elements', $formElements, false);
+                $buttons = $fragment->parse('core/form/submit.php');
+                $buttons = '<fieldset class="rex-form-action">' . $buttons . '</fieldset>';
+
+                // dump($themeid, $addon->getConfig('theme', ''));
+                $configtheme = $addon->getConfig('theme', 'consent_manager_frontend.scss');
+                if ('' === $configtheme) {
+                    $configtheme = 'consent_manager_frontend.scss';
+                }
+                if ($themeid === $configtheme) {
+                    $class = 'edit';
+                    $titleactive = ' - ' . $addon->i18n('config_active_theme');
+                }
+
+                // Ausgabe des Formulars mit csrf-Schutz
+                $fragment = new rex_fragment();
+                $fragment->setVar('class', $class);
+                $fragment->setVar('title', $theme_options['name'] . $titleactive);
+                $fragment->setVar('body', $content, false);
+                $fragment->setVar('buttons', $buttons, false);
+                $output = $fragment->parse('core/page/section.php');
+
+                $output = '
+    <form action="' . rex_url::currentBackendPage() . '#'.str_replace(':', '-', $themeid).'" method="post" id="'.str_replace(':', '-', $themeid).'">
+    <input type="hidden" name="formsubmit" value="1" />
+    <input type="hidden" name="theme" value="' . $themeid . '" />
+        ' . $csrfToken->getHiddenField() . '
+        ' . $output . '
+    </form>
+    ';
+
+                echo $output;
             }
-
-            $formElements = [];
-
-            $n = [];
-            $n['field'] = '';
-
-            $n['field'] .= '<p class="theme_description">';
-            $n['field'] .= '<span>' . $addon->i18n('theme_name') . '</span> ' . $theme_options['name'] . '<br>';
-            $n['field'] .= '<span>' . $addon->i18n('theme_description') . '</span> ' . $theme_options['description'] . '<br>';
-            $n['field'] .= '<span>' . $addon->i18n('theme_type') . '</span> ' . $theme_options['type'] . '<br>';
-            $n['field'] .= '<span>' . $addon->i18n('theme_style') . '</span> ' . $theme_options['style'] . '<br>';
-            $n['field'] .= '<span>' . $addon->i18n('theme_scssfile') . '</span> ' . $themeid . '<br>';
-            $n['field'] .= '<span>' . $addon->i18n('theme_autor') . '</span> ' . implode(', ', $authorlinks) . '<br>';
-            $n['field'] .= '</p>';
-
-            $n['field'] .= '<div class="thumbnail-container" title="' .  $theme_options['name'] . '" data-theme="' . $themeid . '">';
-            $n['field'] .= '  <div class="thumbnail">';
-            $n['field'] .= '   <iframe loading="lazy" width="1440px" height="900px" class="thumbnailframe" src="?page=consent_manager/theme&preview='.$themeid.'&nofocus" data-theme="' . $themeid . '" onload="this.style.opacity = 1"></iframe>';
-            $n['field'] .= '  </div>';
-            $n['field'] .= '</div>';
-
-            $formElements[] = $n;
-
-            $fragment = new rex_fragment();
-            $fragment->setVar('elements', $formElements, false);
-            $content = $fragment->parse('core/form/form.php');
-
-            // Buttons
-            $confirmmsg = $addon->i18n('config_confirm_select', $theme_options['name']);
-            $formElements = [];
-            $n = [];
-            $n['field'] = '<a href="?page=consent_manager/theme&preview=' . $themeid . '" class="btn rex-form-aligned btn-info consent_manager-button-preview" data-theme="' . $themeid . '">' . $addon->i18n('config_btn_preview') . '</a> ';
-            $n['field'] .= '<button class="btn btn-save" type="submit" name="save" data-confirm="' . $confirmmsg . '" value="' . $addon->i18n('config_btn_select', $theme_options['name']) . '">' . $addon->i18n('config_btn_select', $theme_options['name']) . '</button>';
-            $formElements[] = $n;
-            $fragment = new rex_fragment();
-            $fragment->setVar('elements', $formElements, false);
-            $buttons = $fragment->parse('core/form/submit.php');
-            $buttons = '<fieldset class="rex-form-action">' . $buttons . '</fieldset>';
-
-            // dump($themeid, $addon->getConfig('theme', ''));
-            $configtheme = $addon->getConfig('theme', 'consent_manager_frontend.scss');
-            if ('' === $configtheme) {
-                $configtheme = 'consent_manager_frontend.scss';
-            }
-            if ($themeid === $configtheme) {
-                $class = 'edit';
-                $titleactive = ' - ' . $addon->i18n('config_active_theme');
-            }
-
-            // Ausgabe des Formulars mit csrf-Schutz
-            $fragment = new rex_fragment();
-            $fragment->setVar('class', $class);
-            $fragment->setVar('title', $theme_options['name'] . $titleactive);
-            $fragment->setVar('body', $content, false);
-            $fragment->setVar('buttons', $buttons, false);
-            $output = $fragment->parse('core/page/section.php');
-
-            $output = '
-<form action="' . rex_url::currentBackendPage() . '#'.str_replace(':', '-', $themeid).'" method="post" id="'.str_replace(':', '-', $themeid).'">
-<input type="hidden" name="formsubmit" value="1" />
-<input type="hidden" name="theme" value="' . $themeid . '" />
-    ' . $csrfToken->getHiddenField() . '
-    ' . $output . '
-</form>
-';
-
-            echo $output;
         }
     }
 }
