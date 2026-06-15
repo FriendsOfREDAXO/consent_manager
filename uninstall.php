56615f9d7f1ddbc2b40d17b910de434cf72a15bf
Thomas Skerbis <thomas.skerbis@mac.com>
2026-06-15 17:26:32 +0200

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,6 +1,14 @@
 <?php
 
-rex_config::remove('consent_manager', 'config');
+// Alle Konfigurationswerte entfernen, damit Neuinstallationen sauber starten
+// (z. B. damit justInstalled nach einem Reinstall wieder korrekt gesetzt wird).
+if (method_exists('rex_config', 'removeNamespace')) {
+	rex_config::removeNamespace('consent_manager');
+} else {
+	rex_config::remove('consent_manager', 'config');
+	rex_config::remove('consent_manager', 'justInstalled');
+	rex_config::remove('consent_manager', 'forceCache');
+}
 rex_sql_table::get(rex::getTable('consent_manager_cookiegroup'))->drop();
 rex_sql_table::get(rex::getTable('consent_manager_cookie'))->drop();
 rex_sql_table::get(rex::getTable('consent_manager_text'))->drop();
